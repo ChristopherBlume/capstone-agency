c79207b2e8a4d90d750f279efa179bb4ceca056c
Christopher Blume <Christopher.Blume@gmx.de>
2021-01-16 13:35:51 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# capstone-agency
+# Capstone Agency
+
+This is the Capstone Project for the Udacity Fullstack-Developer Nanodegree.
+
+## Accounts to access Application
+
+## Backend
+
+## Frontend
+
+## Postman Collection
---
+++ setup.sh
@@ -0,0 +1,8 @@
+export AUTH0_DOMAIN=""
+export AUTH0_CLIENT_ID = ""
+export API_AUDIENCE=""
+export ALGORITHMS=["RS256"]
+export DATABASE_URL="postgres://JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
+export ENV='development'
+export FLASK_APP=app
+export FLASK_DEBUG=True
