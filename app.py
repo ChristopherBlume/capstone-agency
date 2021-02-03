7d31d86efc2b42371868db52d80b58e69593018c
Christopher Blume <Christopher.Blume@gmx.de>
2021-02-03 15:33:28 +0100

    included new token

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def create_app(test_config=None):
     '''
     @app.route('/')
     def get_greeting():
-        greeting = "Hello" 
+        greeting = "Hello Heroku!" 
         return greeting
 
     '''
