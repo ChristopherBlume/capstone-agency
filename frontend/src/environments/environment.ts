02cfb7a4a26f639cfc5ba9de8845fe8ac5d45b57
Christopher Blume <Christopher.Blume@gmx.de>
2021-02-04 14:25:00 +0100

    changed API url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://cap-agent-backend.herokuapp.com/';
 
 export const environment = {
   production: false,
