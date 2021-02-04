8c04c67a49a840bb067973f7ff89fb3681619008
Christopher Blume <Christopher.Blume@gmx.de>
2021-02-04 11:06:43 +0100

    heroku deployment fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+export const API_URL = 'http://localhost:5000';
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    url: 'capstone-agency.eu', // the auth0 domain prefix
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'cS4oMOvh2bJBdM1i4owjsJVoTqOHHd7C', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
+  }
 };
