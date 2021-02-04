c68272c61df8822dda56b1b1ff4235b03f0c1b70
Christopher Blume <Christopher.Blume@gmx.de>
2021-02-04 13:58:39 +0100

    auth0 heroku login

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     url: 'capstone-agency.eu', // the auth0 domain prefix
     audience: 'http://localhost:5000', // the audience set for the auth0 app
     clientId: 'cS4oMOvh2bJBdM1i4owjsJVoTqOHHd7C', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
+    callbackURL: 'https://cap-agent-backend.herokuapp.com/', // the base url of the running ionic application. 
   }
 };
 
