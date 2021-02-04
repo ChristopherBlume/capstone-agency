11a3e2c41e22d941bdbd619c69347ea5cca3efb1
Christopher Blume <Christopher.Blume@gmx.de>
2021-02-04 13:45:39 +0100

    changed callback URL to heroku backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://cap-agent-backend.herokuapp.com/';
 export const environment = {
   production: true,
   auth0: {
     url: 'capstone-agency.eu', // the auth0 domain prefix
     audience: 'http://localhost:5000', // the audience set for the auth0 app
     clientId: 'cS4oMOvh2bJBdM1i4owjsJVoTqOHHd7C', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
+    callbackURL: 'https://cap-agent-frontend.herokuapp.com/home', // the base url of the running ionic application. 
   }
 };
